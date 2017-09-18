1650ed2e0ea6bdf06a6ba17be17565c642cbc1ac
BlackEnergie <the.perrin@outlook.fr>
2017-09-18 15:35:00 +0200

    TP0

--- TP0Ex5 Comptes Clients/src/TestBanque.java
+++ TP0Ex5 Comptes Clients/src/TestBanque.java
@@ -2,7 +2,7 @@
 public class TestBanque {
 	public static void main(String[] args) {
 		Client Michel = new Client("Michel");
-		Client Jules = new Client("Jules");
+		Client Pierre = new Client("Jules");
 		
 		Compte BNP = new Compte();
 		Compte LCL = new Compte();
@@ -10,12 +10,12 @@ public class TestBanque {
 		
 		Michel.affecterCompte(LCL);
 		Michel.affecterCompte(BNP);
-		Jules.affecterCompte(CMSO);
+		Pierre.affecterCompte(CMSO);
 		BNP.crediter(2000);
 		LCL.crediter(5230);
 		BNP.debiter(3000);
 		Michel.afficher();
-		Jules.afficher();
+		Pierre.afficher();
 		
 	}
 }
